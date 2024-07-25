aec997924e28143adef5151543d938a0ebc07ea5
FrankDestro <frank-destro@outlook.com>
2024-07-25 17:51:44 -0300

    upload project

---
+++ back/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dscatalog.dto;
+
+import com.devsuperior.dscatalog.services.validation.UserInsertValid;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+@UserInsertValid
+public class UserInsertDTO extends UserDTO {
+
+	@NotBlank(message = "Campo Requereido")
+	@Pattern(regexp = "^(?=.*[A-Z])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,}$", message = "A senha deve conter pelo menos uma letra maiúscula e um caractere especial, e ter no mínimo 8 caracteres")
+	private String password;
+
+	UserInsertDTO() {
+		super();
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
